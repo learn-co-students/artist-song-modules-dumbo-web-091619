2f22a14b30f9fae32cb37eb1742e202dc4ba689a
Emirhan Kaplan <emskaplann@gmail.com>
2019-09-23 12:30:05 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,36 +1,30 @@
 require 'pry'
+require_relative './common_methods.rb'
 
-class Song
+
+class ForAll
+  @@classall = []
+  def self.all
+    @@classall
+  end
+end
+
+
+class Song < ForAll
+
+  extend CommonMethods::ClassMethods
+  include CommonMethods::InstanceMethods
+
   attr_accessor :name
   attr_reader :artist
 
-  @@songs = []
-
-  def initialize
-    @@songs << self
-  end
-
   def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
-
-  def self.all
-    @@songs
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
+    @@classall.detect{|a| a.name == name}
   end
 
   def artist=(artist)
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
 end
+
