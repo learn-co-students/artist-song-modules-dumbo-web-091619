ed1cd9a6167dbd5a442051905425f49a0f2130b2
Noam Sauer-Utley <noamsauerutley@gmail.com>
2019-09-23 22:50:11 -0400

    Done.

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,17 @@
 module Memorable
+    module ClassMethods
+        def reset_all
+            all.clear
+        end
 
-   def reset_all
-    all.clear
+        def count
+            self.all.count
+        end  
+    end
+
+  module InstanceMethods
+    def initialize
+        self.class.all << self
+    end
   end
-
-  def count
-    all.count
-  end  
-
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,31 +1,22 @@
 require 'pry'
-require 'concerns/memorable.rb'
 
 class Song
 
-  extend Memorable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods  
+  extend Findable
+  include Paramable
   attr_accessor :name
   attr_reader :artist
 
-  @@songs = []
-
-  def initialize
-    @@songs << self
-  end
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  @@all = []
 
   def self.all
-    @@songs
+    @@all
   end
 
   def artist=(artist)
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
 end
